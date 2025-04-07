adcfe1475401a15a45bc54fb50a706e9b6127f13
OrewaKimi <kimi93.com@gmail.com>
2025-04-07 12:25:55 +0700

    add path for GrocerytItemTile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:grocerytute/components/grocery_item_tile.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -9,33 +11,54 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-        const SizedBox(height: 48),
+          children: [
+            const SizedBox(height: 48),
 
-          // Good morning
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Text("Good Morning,"),
-          ),
+            // Good morning
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text("Good Morning,"),
+            ),
 
-          const SizedBox(height: 4),
+            const SizedBox(height: 4),
 
-          // Let's order fresh items for you 
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Text(
-              "Let's order fresh items for you",
-              style: GoogleFonts.notoSerif(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
+            // Let's order fresh items for you
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Let's order fresh items for you",
+                style: GoogleFonts.notoSerif(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
 
-          // Divider
+            const SizedBox(height: 24),
 
-          // Fresh items + grid
+            // Divider
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Divider(thickness: 4),
+            ),
 
+            const SizedBox(height: 24),
+
+            // Fresh items + grid
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text("Fresh Items", style: TextStyle(fontSize: 16)),
+            ),
+
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return GroceryItemTile(); 
+                },
+              ),
+            ),
           ],
         ),
       ),
