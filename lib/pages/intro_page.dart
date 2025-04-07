8e42a32f840334c0789e46490feac6042f9d755f
OrewaKimi <kimi93.com@gmail.com>
2025-04-07 10:31:50 +0700

    add section fresh items everyday and get started button

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -6,7 +6,49 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        children: [
+          // Logo 
+          Padding(padding: const EdgeInsets.only(
+            left: 80.0, right: 80, top: 80, bottom: 160),
+          child: Image.asset('lib/images/avocado.png'),
+          ),
+
+          // We deliver groceries to your door step
+          const Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Text(
+              'We deliver groceries to your door step',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+          // Fresh items everyday
+          Text('Fresh Items Everyday',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey[700],
+            ),
+          ),
+
+          // get started button
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            padding: EdgeInsets.all(24),
+            child: Text(
+              'Get Started',
+            style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      )
     );
   }
 }
