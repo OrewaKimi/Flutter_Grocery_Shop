9a50c76ee2a97f9f0aad4da8d936b7ddbb7e2ad8
OrewaKimi <kimi93.com@gmail.com>
2025-04-07 11:49:08 +0700

    update section good morning and lets order fresh items

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        const SizedBox(height: 48),
+
+          // Good morning
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text("Good Morning,"),
+          ),
+
+          const SizedBox(height: 4),
+
+          // Let's order fresh items for you 
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              "Let's order fresh items for you",
+              style: GoogleFonts.notoSerif(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          // Divider
+
+          // Fresh items + grid
+
+          ],
+        ),
+      ),
+    );
   }
 }
